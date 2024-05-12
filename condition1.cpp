606525bf7f6ec93d14d4af2585c6bb964c6a3141
Amay Saxena <amaysaxena8@gmail.com>
2024-05-12 20:47:35 +0530

    1st commit

---
+++ condition1.cpp
@@ -0,0 +1,17 @@
+#include<iostream>
+using namespace std;
+int main()
+{
+    int a,b;
+    cout<<"Enter a: ";
+    cin>>a;
+    cout<<"\nEnter b: ";
+    cin>>b;
+    if(a>b){
+        cout<<"\nAnswer is a";
+    }
+    else
+        cout<<"\nAnswer is b";
+    
+    return 0;
+}
