1c68fd44257b6d5a32a2d179c815d5e6ce2346c1
Amay Saxena <amaysaxena8@gmail.com>
2024-05-14 12:00:21 +0530

    Pattern using while loop 1

--- pattern1.cpp
+++ pattern1.cpp
@@ -2,9 +2,18 @@
 using namespace std;
 int main()
 {
-    int i=4,j;
-    while(j<=i)
+    int i=1;
+    int n;
+    cin>>n;
+    while(i<=n)
     {
-       
+        int j=1;
+       while(j<=n){
+        cout<<"*";
+        j++;
+       }
+       cout<<endl;
+       i++;
     }
+    return 0;
 }
