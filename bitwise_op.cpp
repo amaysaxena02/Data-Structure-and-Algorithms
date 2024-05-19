a5a8c4271b085e9c38e69eb76be02e2afbd31859
Amay Saxena <amaysaxena8@gmail.com>
2024-05-19 11:12:15 +0530

    Bitwise Operators 1

--- bitwise_op.cpp
+++ bitwise_op.cpp
@@ -3,12 +3,18 @@ using namespace std;
 
 int main()
 {
-    int a=4;
-    int b=6;
+    int a,b;
+    
+    cout<<"Enter a: "<<endl;
+    cin>>a;
+    cout<<"Enter b: "<<endl;
+    cin>>b;
+    
 
     cout<<"a&b: "<<(a&b)<<endl;
     cout<<"a|b: "<<(a|b)<<endl;
     cout<<"~a: "<<~a<<endl;
+    cout<<"~b: "<<~b<<endl;
     cout<<"a^b: "<<(a^b)<<endl;
      
     return 0;
