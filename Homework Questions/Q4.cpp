b7f9f088188a1c12912a41413672cc19e81567ff
Amay Saxena <amaysaxena8@gmail.com>
2024-05-23 13:06:26 +0530

    Homework Q4

---
+++ Homework Questions/Q4.cpp
@@ -0,0 +1,11 @@
+#include<iostream>
+using namespace std;
+
+int main()
+{
+    int num = 3;
+    cout<<(25*(++num));
+
+    return 0;
+}
+//pre increment operator, increased value of 'num' to 4 before the execution of statement.
