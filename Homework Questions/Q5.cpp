6d113111803686a5fb17de5501a9ef48f97b8998
Amay Saxena <amaysaxena8@gmail.com>
2024-05-23 20:46:57 +0530

    Homework Q5

---
+++ Homework Questions/Q5.cpp
@@ -0,0 +1,16 @@
+#include<iostream>
+using namespace std;
+
+int main()
+{
+    int a = 1;
+    int b = a++;
+    int c = ++a;
+    
+    cout<<a<<endl;
+    cout<<b<<endl;
+    cout<<c;
+
+    return 0;
+
+}
