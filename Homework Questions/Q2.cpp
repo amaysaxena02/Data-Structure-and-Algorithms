230b99027fa3c04c7ad8f6a2799866bb82784f27
Amay Saxena <amaysaxena8@gmail.com>
2024-05-23 12:44:59 +0530

    Q2

---
+++ Homework Questions/Q2.cpp
@@ -0,0 +1,21 @@
+#include<iostream>
+using namespace std;
+
+int main()
+{
+    int a=1,b=2;
+
+    if (a-- > 0 && ++b > 2)
+    {
+        cout<<"Stage1 - Inside IF";
+    }
+    else
+    {
+        cout<<"Stage2 - Inside ELSE";
+    }
+
+    return 0;
+}
+//a have post decrement which means a will change after the statement executes so it will have its original value when if statement gets executed.
+//b have pre increment which means its value will increase while the if statement gets executes.
+//due to these operations, if condition satisfies and we get output from if block.
